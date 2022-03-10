d46c805155f05283383e16834d2dcec4039b69c9
FirEmerald <mccaslinwalt@gmail.com>
2022-03-09 20:09:23 -0600

    Update MC4API.java
    fix using older MC4API class

--- src/main/java/firemerald/mc4/api/MC4API.java
+++ src/main/java/firemerald/mc4/api/MC4API.java
@@ -3,10 +3,18 @@ package firemerald.mc4.api;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.minecraft.util.ResourceLocation;
+
 public class MC4API
 {
-	public static final String API_VERSION = "0.0.1";
-	public static final Logger LOGGER = LogManager.getLogger("MC4 MCMSAPI");
+	public static final String API_VERSION = "0.0.0";
+	public static final Logger LOGGER = LogManager.getLogger("MC4 API");
 
 	public static final String MOD_ID = "mc4";
+	public static final ResourceLocation MC4_LOCKABLE_CAP_NAME = new ResourceLocation(MOD_ID, "lockable"); //used for custom setting of capabilities
+	public static final ResourceLocation MC4_SCANABLE_CAP_NAME = new ResourceLocation(MOD_ID, "scanable"); //used for custom setting of capabilities
+	public static final ResourceLocation MC4_DAMAGE_SPECIFIC_CAP_NAME = new ResourceLocation(MOD_ID, "damage_specific"); //used for custom setting of capabilities
+	public static final ResourceLocation MC4_GRAVITY_WELL_CAP_NAME = new ResourceLocation(MOD_ID, "gravity_well"); //used for custom setting of capabilities
+	public static final ResourceLocation MC4_LIGHT_AURA_CAP_NAME = new ResourceLocation(MOD_ID, "light_aura"); //used for custom setting of capabilities
+	public static final ResourceLocation MC4_WORLD_PROPS_NAME = new ResourceLocation(MOD_ID, "world_props"); //used for custom setting of capabilities
 }
